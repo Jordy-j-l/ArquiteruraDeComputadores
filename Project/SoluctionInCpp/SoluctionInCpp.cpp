e710e82dd7fe43cf7bab76bac57b18afd2ee0d22
Inesar16 <ines.arodrigues16@gmail.com>
2025-04-23 13:10:15 +0100

    inicio de assembly in line

--- Project/SoluctionInCpp/SoluctionInCpp.cpp
+++ Project/SoluctionInCpp/SoluctionInCpp.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define size1 3
+#define size1 5
 #define size2 4
 
 int SumTotal(char arrayOne[], char arrayTwo[]) {
@@ -28,11 +28,43 @@ int main() {
     char one[size1];
     char two[size2];
 
-    for (char i = 0; i < size1; i++) {
+    /*for (char i = 0; i < size1; i++) {
         one[i] = i + 1;
     }
     for (char i = 0; i < size2; i++) {
         two[i] = i + 2;
+    }*/
+
+    
+
+    _asm {
+    SumTotal:
+
+    mov eax, 0
+    mov ecx, 0
+    for1:
+        add ecx, 1
+        mov one[eax], cl
+        inc eax
+        mov ecx, eax
+        cmp eax, size1
+        jb for1 
+    mov eax, 0
+    mov ecx, 0
+    for2:
+        add ecx, 2
+        mov two[eax], cl
+        inc eax
+        mov ecx, eax
+        cmp eax, size2
+        jb for2
+    }
+    for (char i = 0; i < size1; i++) {
+        printf("One=%d\n", one[i]);
+    }
+    for (char i = 0; i < size2; i++) {
+        printf("Two=%d\n", two[i]);
     }
     printf("A media dos dois arrays e: %d\n", SumTotal(one, two));
 }
+
