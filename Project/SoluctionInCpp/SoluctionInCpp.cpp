a9a11cb0b8f83b811d6b11080bfb312823957bd6
Jordy <122643228+Jordy-j-l@users.noreply.github.com>
2025-04-25 19:07:51 +0100

    Correção alinhada com o pedido em SIMD

--- Project/SoluctionInCpp/SoluctionInCpp.cpp
+++ Project/SoluctionInCpp/SoluctionInCpp.cpp
@@ -1,38 +1,68 @@
-#include <iostream>
-#define size1 3
-#define size2 4
+    #include <iostream>
+    #define size1 3
+    #define size2 4
 
-int SumTotal(char arrayOne[], char arrayTwo[]) {
-    int maxSize = size1 + size2 == 0 ? 2 : size1 + size2;// Se ambos os tamanhos forem 0, usa 2 como valor mínimo porque cada array tera 1 elemento
-    int sumTotal = 0;
+    int SumTotal(char arrayOne[], char arrayTwo[]) {
+        int maxSize = size1 + size2 == 0 ? 2 : size1 + size2;// Se ambos os tamanhos forem 0, usa 2 como valor mínimo porque cada array tera 1 elemento
+        int sumTotal = 0;
+        if (size1 > size2) {
+            char tree[size1];
+            for (char i = 0;i < size1) {
+                tree[i] = arrayOne[i] + arrayTwo[i];
+            }
+            for (char i = 0;i < size2) {
+                sumTotal += tree[i];
+            }
+        }else {
+            char tree[size2];
+            for (char i = 0;i<size2) {
+                tree[i] = arrayOne[i] + arrayTwo[i];
+            }
+            for (char i = 0;i < size2) {
+                sumTotal += tree[i];
+            }
+        }
 
-    for (int i = 0; i < maxSize; i++) {
-        if (i < size1 && i < size2) {//ciclo if para confirmar se o i está diacordo com os size
-            sumTotal += arrayOne[i] + arrayTwo[i];
-        }
-        else if (i < size1) {
-            sumTotal += arrayOne[i];
-        }
-        else if (i < size2) {
-            sumTotal += arrayTwo[i];
+        return sumTotal / maxSize;
+    }
+
+    int main() {
+        char one[size1];
+        char two[size2];
+
+        if (size1>size2){
+            char tree[size1]
+            for (char i = 0; i < size1; i++) {
+                    one[i] = i + 1;
+                }
+            for (char i = 0; i < size1; i++) {
+                
+                    if (i <= size2) {
+                        two[i] = i + 2;
+                    }
+                    else {
+                        two[i] = 0;
+                    }
+
+                }
         }
         else {
-            break;
+            char tree[size2]
+            for (char i = 0; i < size2; i++) {
+                if (i <= size1) {
+                    one[i] = i+1;
+                }
+                else {
+                    one[i] = 0;
+                }
+            }
+            for (char i = 0; i < size1; i++) {
+                    two[i] = i + 2;
+            }
         }
-    }
+        }
 
-    return sumTotal / maxSize;
-}
 
-int main() {
-    char one[size1];
-    char two[size2];
-
-    for (char i = 0; i < size1; i++) {
-        one[i] = i + 1;
+    
+        printf("A media dos dois arrays e: %d\n", SumTotal(one, two));
     }
-    for (char i = 0; i < size2; i++) {
-        two[i] = i + 2;
-    }
-    printf("A media dos dois arrays e: %d\n", SumTotal(one, two));
-}
