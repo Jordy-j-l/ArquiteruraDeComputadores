4b5c3ad50cda9242e0fab75526e18423931cce40
Jordy <122643228+Jordy-j-l@users.noreply.github.com>
2025-04-25 20:05:35 +0100

    Start Solution em Assembly

--- Project/SoluctionInAssembly/ProjectAssembly/ProjectAssembly.cpp
+++ Project/SoluctionInAssembly/ProjectAssembly/ProjectAssembly.cpp
@@ -1,20 +1,33 @@
-// ProjectAssembly.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+int main() {
 
-#include <iostream>
+    if (size1 > size2) {
+        char one[size1];
+        char two[size1];
+        for (char i = 0; i < size1; i++) {
+            one[i] = -i;
+            printf("%d >>\n", one[i]);
+        }
+        for (char i = 0; i < size1; i++) {
 
-int main()
-{
-    std::cout << "Hello World!\n";
+            two[i] = i < size2 ? i + 2 : 0;
+            printf("%d >>\n", two[i]);
+        }
+        printf("A media dos dois arrays e: %d\n", SumTotal(one, two));
+    }
+    else {
+        char one[size2];
+        char two[size2];
+
+        for (char i = 0; i < size2; i++) {
+            one[i] = i < size1 ? -i : 0;
+            printf("%d >>\n", one[i]);
+
+        }
+        for (char i = 0; i < size2; i++) {
+            two[i] = i + 2;
+            printf("%d >>\n", two[i]);
+        }
+        printf("A media dos dois arrays e: %d\n", SumTotal(one, two));
+    }
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
