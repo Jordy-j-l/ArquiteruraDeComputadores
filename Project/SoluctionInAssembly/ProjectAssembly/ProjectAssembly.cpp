bbc3657af3ee3ec22496757abe74250f0d7554fe
Jordy <122643228+Jordy-j-l@users.noreply.github.com>
2025-04-26 09:34:10 +0100

    commit

--- Project/SoluctionInAssembly/ProjectAssembly/ProjectAssembly.cpp
+++ Project/SoluctionInAssembly/ProjectAssembly/ProjectAssembly.cpp
@@ -1,33 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#define size1 8
+#define size2 6
+#define MAIOR (size1 > size2 ? size1 : size2)
+
+
+
 int main() {
+    int maxSize = size1 + size2 == 0 ? 2 : size1 + size2;// Se ambos os tamanhos forem 0, usa 2 como valor mínimo porque cada array tera 1 elemento
+    char one[MAIOR];
+    char two[MAIOR];
 
-    if (size1 > size2) {
-        char one[size1];
-        char two[size1];
-        for (char i = 0; i < size1; i++) {
-            one[i] = -i;
-            printf("%d >>\n", one[i]);
-        }
-        for (char i = 0; i < size1; i++) {
+    for (int i = 0; i < MAIOR; i++) {
 
-            two[i] = i < size2 ? i + 2 : 0;
-            printf("%d >>\n", two[i]);
-        }
-        printf("A media dos dois arrays e: %d\n", SumTotal(one, two));
+        one[i] = i < size1 ? i + 1 : 0;
+        printf("%d >>\n", one[i]);
     }
-    else {
-        char one[size2];
-        char two[size2];
+    for (int i = 0; i < MAIOR; i++) {
 
-        for (char i = 0; i < size2; i++) {
-            one[i] = i < size1 ? -i : 0;
-            printf("%d >>\n", one[i]);
-
-        }
-        for (char i = 0; i < size2; i++) {
-            two[i] = i + 2;
-            printf("%d >>\n", two[i]);
-        }
-        printf("A media dos dois arrays e: %d\n", SumTotal(one, two));
+        two[i] = i < size2 ? i + 2 : 0;
+        printf("%d >>\n", two[i]);
     }
+    printf("A media dos dois arrays e: %d\n", SumTotal(one, two, maxSize));
+
+
+
 
 }
