3b4b688132ff11e06a175ae7e21bf56dad8f051f
Jordy <122643228+Jordy-j-l@users.noreply.github.com>
2025-05-03 16:11:21 +0100

    teste

--- Project/ProjectASMExrc5/ProjectASMExrc5.cpp
+++ Project/ProjectASMExrc5/ProjectASMExrc5.cpp
@@ -1,41 +1,50 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <chrono>
 
-#define size 20
+#define SIZE 20
+extern "C" void Sinal(char one[], char two[], char result[], int arraySize);
+// Função simplificada para comparar magnitudes
+void SinalSimples(char A[], char B[], char R[], int n) {
+    for (int i = 0; i < n; i++) {
+        // Calcula valores absolutos
+        char absA = A[i] < 0 ? -A[i] : A[i];
+        char absB = B[i] < 0 ? -B[i] : B[i];
 
-
-extern "C" void Sinal( char one[],  char two[],  char result[], int arraySize);
-
-
+        // Compara e armazena o resultado
+        if (absA > absB) {
+            R[i] = A[i] < 0 ? -1 : 1;
+        }
+        else if (absB > absA) {
+            R[i] = B[i] < 0 ? -1 : 1;
+        }
+        else {
+            R[i] = 0;
+        }
+    }
+}
 
 int main() {
-    // Aloca memória para os arrays
-     char one[size];
-     char two[size];
-     char resultASM[size];
-     char resultC[size];
+    char A[SIZE], B[SIZE], ResultadoC[SIZE], ResultadoASM[SIZE];
 
+    // Inicializa gerador de números aleatórios
     srand(time(NULL));
 
-    // Preenche os arrays com valores aleatórios entre 0 e 255
-    for (int i = 0; i < size; i++) {
-        one[i] = (rand() % 256) - 128;
-
+    // Preenche os arrays com valores entre -128 e 127
+    for (int i = 0; i < SIZE; i++) {
+        A[i] = (rand() % 256) - 128;
+        B[i] = (rand() % 256) - 128;
     }
-    for (int i = 0; i < size; i++) {
-        two[i] = (rand() % 256) - 128;
 
+    // Chama a função
+    SinalSimples(A, B, ResultadoC, SIZE);
+    Sinal(A, B, ResultadoASM, SIZE);
+    // Mostra os resultados
+    printf("A\tB\tResultadoemC\tResultadoemASM\n");
+    printf("-----------------------\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d\t%d\t%d\t%d\n", A[i], B[i], ResultadoC[i], ResultadoASM[i]);
     }
-    Sinal(one, two, resultASM, size);
 
-    // Mostrar os resultados de alguns elementos para verificação
-    printf("Resultados:\n");
-    for (int i = 0; i < size; i++) {
-        printf("ArayOne[%d] = % d\t ArayTwo[%d] = % d |\tASM[%d] = %d\n", i, one[i], i, two[i], i, resultASM[i]);
-    }
-    system("pause");
     return 0;
 }
--- Project/ProjectASMExrc5/asmExerc5.asm
+++ Project/ProjectASMExrc5/asmExerc5.asm
@@ -68,3 +68,4 @@ done:
 Sinal ENDP
 
 END
+
