d6de1c106e2b2884ce468acc5c9744916007a2ac
Jordy <122643228+Jordy-j-l@users.noreply.github.com>
2025-05-03 15:52:44 +0100

    Teste de guardar sinal

--- Project/ProjectASMExrc5/asmExerc5.asm
+++ Project/ProjectASMExrc5/asmExerc5.asm
@@ -1,109 +1,70 @@
 .MODEL FLAT, C
 .CODE
 
-; Função auxiliar: abs_complement - converte para complemento se negativo
-; Entrada: AL = valor
-; Saída: AL = valor absoluto em complemento
-abs_complement PROC
-    test al, al
-    jns positive
-    neg al          ; Complemento de 2 para números negativos
-positive:
-    ret
-abs_complement ENDP
+; void Sinal(char *A, char *B, char *R, int n);
+Sinal PROC
+    push    ebp
+    mov     ebp, esp
+    push    ebx
+    push    esi
+    push    edi
 
-average_arrays_signed PROC
-    push ebp
-    mov ebp, esp
-    push esi
-    push edi
-    push ebx
-    push ecx
-    push edx
-    
-    mov esi, [ebp+8]    ; array one
-    mov edi, [ebp+12]   ; array two
-    mov edx, [ebp+16]   ; result
-    mov ebx, [ebp+20]   ; signs array
-    mov ecx, [ebp+24]   ; size
-    
-    xor eax, eax        ; índice i = 0
-    
-process_signs:
-    cmp eax, ecx
-    jge process_averages
-    
-    ; Determinar sinal do maior em módulo
-    mov bl, [esi+eax]   ; one[i]
-    mov bh, [edi+eax]   ; two[i]
-    
-    ; Calcular valores absolutos para comparação
-    mov dl, bl
-    neg dl
-    cmovl dl, bl        ; dl = abs(bl)
-    
-    mov dh, bh
-    neg dh
-    cmovl dh, bh        ; dh = abs(bh)
-    
-    ; Comparar magnitudes
-    cmp dl, dh
-    jge use_one_sign
-    
-    ; Usar sinal de two[i]
-    mov bl, bh
-    
-use_one_sign:
-    ; Armazenar sinal (BL tem o valor com sinal do maior)
-    sar bl, 7           ; Extende sinal: 0xFF se negativo, 0x00 se positivo
-    or bl, 1            ; Converte para -1 ou +1
-    mov [ebx+eax], bl   ; signs[i] = (maior < 0) ? -1 : 1
-    
-    ; Converter para complemento se negativo
-    mov bl, [esi+eax]
-    call abs_complement
-    mov [esi+eax], al
-    
-    mov bl, [edi+eax]
-    call abs_complement
-    mov [edi+eax], al
-    
-    inc eax
-    jmp process_signs
+    mov     esi, [ebp+8]    ; esi = A
+    mov     ebx, [ebp+12]   ; ebx = B
+    mov     edi, [ebp+16]   ; edi = R
+    mov     ecx, [ebp+20]   ; ecx = n
+    xor     edx, edx        ; edx = i = 0
 
-process_averages:
-    ; Chamar função original de média
-    push ecx            ; size
-    push edx            ; result
-    push edi            ; array two
-    push esi            ; array one
-    call average_arrays
-    add esp, 16
-    
-apply_signs:
-    xor eax, eax        ; reset índice
-    
-apply_loop:
-    cmp eax, ecx
-    jge done
-    
-    ; Multiplicar pelo sinal
-    mov bl, [ebx+eax]   ; signs[i]
-    mov dl, [edx+eax]   ; result[i]
-    imul dl, bl         ; result[i] *= signs[i]
-    mov [edx+eax], dl
-    
-    inc eax
-    jmp apply_loop
+loop_start:
+    cmp     edx, ecx
+    jg     done
+
+    mov     al, [esi+edx]   ; al = A[i]
+    mov     ah, [ebx+edx]   ; ah = B[i]
+    mov cl,al
+    mov ch,ah
+    cmp al,0
+    jge CompararB
+    neg al
+
+    CompararB:
+        cmp ah,0
+        jge cmp_vals
+        neg al
+    cmp_vals:
+
+    cmp     al, ah
+    jl      storeA
+    jg      storeB
+    mov     byte ptr [edi+edx], 0
+    jmp     next_iter
+
+storeA:
+    cmp     cl, 0
+    jge     storeA_pos
+    mov     byte ptr [edi+edx], -1
+    jmp     next_iter
+storeA_pos:
+    mov     byte ptr [edi+edx], 1
+    jmp  next_iter
+storeB:
+    cmp     ch, 0
+    jge     storeB_pos
+    mov     byte ptr [edi+edx], -1
+    jmp     next_iter
+ storeB_pos:
+    mov     byte ptr [edi+edx], 1   
+
+next_iter:
+    inc     edx
+    jmp     loop_start
 
 done:
-    pop edx
-    pop ecx
-    pop ebx
-    pop edi
-    pop esi
-    pop ebp
+    pop     edi
+    pop     esi
+    pop     ebx
+    pop     ebp
     ret
-average_arrays_signed ENDP
+Sinal ENDP
 
 END
