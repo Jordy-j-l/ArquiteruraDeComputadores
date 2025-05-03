2a725cf5c925a5c75774135efbd2f13d9866a32f
Jordy <122643228+Jordy-j-l@users.noreply.github.com>
2025-05-03 17:23:59 +0100

    C a funcionar

---
+++ Project/ProjectASMCinco/Asm.asm
@@ -0,0 +1,79 @@
+﻿.686
+.MODEL FLAT,C
+.STACK 4096
+
+PUBLIC Sinal
+.CODE
+
+; void Sinal(char *A, char *B, char *R, int n);
+Sinal PROC C
+    push    ebp
+    mov     ebp,esp
+    push    ebx
+    push    esi
+    push    edi
+
+    mov     esi,[ebp+8]    ; A
+    mov     edi,[ebp+12]   ; B
+    mov     ebx,[ebp+16]   ; R
+    mov     ecx,[ebp+20]   ; n
+    xor     edx,edx        ; i=0
+
+.loop:
+    cmp     edx,ecx
+    jge     .done
+
+    mov     al,[esi+edx]   ; A[i]
+    mov     bl,[edi+edx]   ; B[i]
+
+    ; |A[i]| → AH
+    mov     ah,al
+    cmp     ah,0
+    jge     .absA_ok
+    neg     ah
+.absA_ok:
+
+    ; |B[i]| → BH
+    mov     bh,bl
+    cmp     bh,0
+    jge     .compare
+    neg     bh
+.compare:
+
+    cmp     ah,bh
+    ja      .storeA
+    jb      .storeB
+
+    mov     byte ptr [ebx+edx],0
+    jmp     .next
+
+.storeA:
+    cmp     al,0
+    jl      .storeA_neg
+    mov     byte ptr [ebx+edx],1
+    jmp     .next
+.storeA_neg:
+    mov     byte ptr [ebx+edx],-1
+    jmp     .next
+
+.storeB:
+    cmp     bl,0
+    jl      .storeB_neg
+    mov     byte ptr [ebx+edx],1
+    jmp     .next
+.storeB_neg:
+    mov     byte ptr [ebx+edx],-1
+
+.next:
+    inc     edx
+    jmp     .loop
+
+.done:
+    pop     edi
+    pop     esi
+    pop     ebx
+    pop     ebp
+    ret
+Sinal ENDP
+
+END Sinal
--- Project/ProjectASMCinco/ProjectASMCinco.cpp
+++ Project/ProjectASMCinco/ProjectASMCinco.cpp
@@ -1,20 +1,56 @@
-// ProjectASMCinco.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#include <iostream>
+#define SIZE 20
+//extern "C" void Sinal(char one[], char two[], char result[], int arraySize);
+// Função simplificada para comparar magnitudes
+void SinalSimples(char A[], char B[], char R[], int n) {
+    for (int i = 0; i < n; i++) {
+        // Calcula valores absolutos
+        char absA = A[i] < 0 ? -A[i] : A[i];
+        char absB = B[i] < 0 ? -B[i] : B[i];
 
-int main()
-{
-    std::cout << "Hello World!\n";
+        // Compara e armazena o resultado
+        if (absA > absB) {
+            R[i] = A[i] < 0 ? -1 : 1;
+        }
+        else if (absB > absA) {
+            R[i] = B[i] < 0 ? -1 : 1;
+        }
+        else if (absA == absB) {
+            if ((A[i] == -B[i]) || -A[i] == B[i]) {
+                R[i] = 0;
+            }
+        }
+        else {
+            R[i] = 0;
+        }
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    char A[SIZE], B[SIZE], ResultadoC[SIZE], ResultadoASM[SIZE];
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    // Inicializa gerador de números aleatórios
+    srand(time(NULL));
+
+    // Preenche os arrays com valores entre -128 e 127
+    for (int i = 0; i < SIZE; i++) {
+        A[i] = (rand() % 256) - 128;
+        B[i] = (rand() % 256) - 128;
+    }
+
+
+    // Chama a função
+    SinalSimples(A, B, ResultadoC, SIZE);
+    //Sinal(A, B, ResultadoASM, SIZE);
+    // Mostra os resultados
+    printf("A\tB\tResultadoemC\tResultadoemASM\n");
+    printf("-----------------------\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d\t%d\t%d\t\n", A[i], B[i], ResultadoC[i]);
+    }
+
+    return 0;
+}
